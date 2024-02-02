c9cb9ce01e02c3c9c0852262b48cad4f19e60913
GreatJang <1wkdwnsgur@gmail.com>
2024-02-02 17:53:47 +0900

    SOM-22-boardUpdate

--- board/src/main/java/com/encore/board/Post/controller/IndexController.java
+++ board/src/main/java/com/encore/board/Post/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.encore.board.controller;
+package com.encore.board.Post.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
---
+++ board/src/main/java/com/encore/board/Post/service/PostService.java
@@ -0,0 +1,7 @@
+package com.encore.board.Post.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+}
